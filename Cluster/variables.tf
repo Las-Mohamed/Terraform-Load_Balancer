262994f6d8c02bb18f50b1bdd4c5675b9c4fbd46
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2023-12-15 16:26:18 +0100

    Update variables.tf

--- Cluster/variables.tf
+++ Cluster/variables.tf
@@ -20,5 +20,5 @@ variable "Object_id" {
   type = string
   description = "Oject_id is longer returned with data source instead use it as a declared variable" 
   # (to find it use in Cli "az account show --query "{subscriptionId:id, tenantId:tenantId, objectId:id})"
-  default = "393e3de3-0900-4b72-8f1b-fb3b1d6b97f1"
+  default = "393e3de3-0900-4b72-8f1b-XXXXXXXX"
 }
