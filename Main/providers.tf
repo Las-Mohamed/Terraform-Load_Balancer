c6ee93ade3ce9c47f1a172c6f467068b9f8feecb
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2024-01-03 14:47:26 +0100

    Update providers.tf

--- Main/providers.tf
+++ Main/providers.tf
@@ -11,6 +11,6 @@ terraform {
 }
 
 provider "azurerm" {
-# You can add your credentials here but the Best Practice is to use an external programm that will store your credentials - In this case Dl Azure CLI and authentificate
+# You can add your credentials here but the Best Practice for security purpose is to use an external programm that will store your credentials - In this case Dl Azure CLI and authentificate
   features {}
 }
